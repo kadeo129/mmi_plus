1a51f56f8d8434bf219568f19ae97d5fcd347778
kadeo <kadeo129@gmail.com>
2016-11-01 16:40:13 +0100

    ok avt modif templates FOSUserBundle

--- src/MMI/TVBundle/Entity/User.php
+++ src/MMI/TVBundle/Entity/User.php
@@ -23,6 +23,58 @@ class User extends BaseUser
     protected $id;
 
     /**
+     * @ORM\OneToMany(targetEntity="MMI\TVBundle\Entity\Video", mappedBy="user", cascade={"remove"})
+     */
+    private $videos;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="studentNumber", type="string", length=255)
+     */
+    private $studentNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Le nom est trop court",
+     *     maxMessage="Le nom est trop long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255)
+     *
+     * * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Le prénom est trop court",
+     *     maxMessage="Le prénom est trop long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photo", type="string", length=255)
+     */
+    private $photo;
+
+
+    /**
      * Get id
      *
      * @return int
@@ -31,11 +83,6 @@ class User extends BaseUser
     {
         return $this->id;
     }
-
-    /**
-     * @ORM\OneToMany(targetEntity="MMI\TVBundle\Entity\Video", mappedBy="user", cascade={"remove"})
-     */
-    private $videos;
 
     /**
      * Add video
@@ -69,5 +116,101 @@ class User extends BaseUser
     public function getVideos()
     {
         return $this->videos;
+    }
+
+    /**
+     * Set studentNumber
+     *
+     * @param string $studentNumber
+     *
+     * @return User
+     */
+    public function setStudentNumber($studentNumber)
+    {
+        $this->studentNumber = $studentNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get studentNumber
+     *
+     * @return string
+     */
+    public function getStudentNumber()
+    {
+        return $this->studentNumber;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set photo
+     *
+     * @param string $photo
+     *
+     * @return User
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
     }
 }
