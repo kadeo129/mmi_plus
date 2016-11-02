63ab6e2455b72cc557fd33a7c37e4f8f7c793de0
kadeo <kadeo129@gmail.com>
2016-11-02 12:01:36 +0100

    new commit

--- README.md
+++
@@ -1,4 +0,0 @@
-.checkout
-=========
-
-A Symfony project created on October 3, 2016, 11:22 am.
--- src/MMI/TVBundle/Controller/UserController.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Belles Bulles
- * Date: 01/11/2016
- * Time: 16:23
- */
